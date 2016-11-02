7333c08acfbb79c6bac7e7f516cea881648b956b
cwfroom <cwfroom123@gmail.com>
2016-11-01 19:40:38 -0700

    simple scene setup

---
+++ SnowFight/SnowFight/Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void GotoGame()
+    {
+        SceneManager.LoadScene("Level");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
