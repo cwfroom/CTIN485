2f58120ce55de7fbd27a5dd6b28d6535807ea79e
Wenfei Cao <admin@cwf123.com>
2016-11-08 15:47:47 -0800

    start building player

---
+++ SnowFight/SnowFight/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehavior : MonoBehaviour {
+    GameManager gmr;
+    private bool online = false;
+
+	// Use this for initialization
+	void Start () {
+        gmr = FindObjectOfType<GameManager>();
+        if (gmr)
+        {
+            online = true;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
