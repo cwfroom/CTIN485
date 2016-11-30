07fec7a098e16cca6ad9ca1d2bc304a75dfa4562
cwfroom <cwfroom123@gmail.com>
2016-11-29 21:27:05 -0800

    change FPS prefab

--- SnowFight/SnowFight/Assets/Scripts/PlayerBehavior.cs
+++ SnowFight/SnowFight/Assets/Scripts/PlayerBehavior.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class PlayerBehavior : MonoBehaviour {
     GameManager gmr;
-    //private bool online = false;
+    private bool online = false;
 
     private Vector3 pos;
 
@@ -12,7 +12,7 @@ public class PlayerBehavior : MonoBehaviour {
         gmr = FindObjectOfType<GameManager>();
         if (gmr)
         {
-            //online = true;
+            online = true;
             transform.position = gmr.SpawnPoints[gmr.PlayerID];
         }
         pos = transform.position;
@@ -21,7 +21,7 @@ public class PlayerBehavior : MonoBehaviour {
     
     // Update is called once per frame
     void Update () {
-        if (pos != transform.position)
+        if (pos != transform.position && online)
         {
             gmr.nm.SendPos(transform.position, transform.rotation);
             //gmr.SendPos(transform.position);
